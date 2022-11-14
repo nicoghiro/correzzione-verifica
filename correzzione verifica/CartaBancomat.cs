8f4680f4af10e6237a613c35b79b323dba3a0877
nicoghiro <nicolasghirardi251005@gmail.com>
2022-11-14 16:16:07 +0100

    implementazione carta bancomat

--- correzzione verifica/CartaBancomat.cs
+++ correzzione verifica/CartaBancomat.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace correzzione_verifica
 {
-    internal class CartaBancomat
+    public class CartaBancomat
     {
+        string id;
+        string pin;
+        ContoCorrente conto;
+        public CartaBancomat(string id, string pin, ContoCorrente conto)
+        {
+            this.id = id;
+            this.pin = pin;
+            this.conto = conto;
+        }   
+        public string getID()
+        {
+            return id;  
+        }
+        public void deposita(float value,string Pin)
+        {
+            if (pin == Pin)
+            {
+               conto.Deposito(value);
+            }
+            else
+            {
+                throw new Exception("pin sbagliato");
+            }
+        }
+        public void preleva(float value, string Pin)
+        {
+            if (pin == Pin)
+            {
+                conto.Prelievo(value);
+            }
+            else
+            {
+                throw new Exception("pin sbagliato");
+            }
+        }
+        public float getSaldo()
+        {
+            return conto.getSaldo();
+        }
     }
 }
